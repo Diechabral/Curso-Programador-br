431f12b76f1bd775623e2dde4cdfeebc5b7e08aa
Diego Cabral <91792621+Diechabral@users.noreply.github.com>
2021-10-27 19:27:50 -0300

    update

--- 006.5 JavaScript - Básico copy 2/script.js
+++
@@ -1,4 +0,0 @@
-var numero = 8;
-var str = "valor 'texto entre aspas ' qualquer " + numero;
-console.log(str);
-// string so funcionaa com concatenação
---
+++ 006.5 JavaScript - Básico/script.js
@@ -0,0 +1,5 @@
+var numero = "88" * 2;
+var str = "valor qualquer " * 2;
+console.log(str);
+console.log(numero)
+// string so funcionaa com concatenação
