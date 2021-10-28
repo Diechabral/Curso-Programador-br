c1f8ae3abfc7274e9205c36a1ba04fa7d79ad584
Diego Cabral <91792621+Diechabral@users.noreply.github.com>
2021-10-27 21:48:21 -0300

    update

--- 006.10 JavaScript - Básico/script.js
+++ 006.10 JavaScript - Básico/script.js
@@ -1,9 +1,31 @@
-var idade = 19;
+var nota1 = 2;
+var nota2 = 6.0;
 
-if (idade < 18 || idade > 70) {
-  console.log("pode");
-  console.log("qual o seu pedido");
+var media = (nota1 + nota2) / 2;
+
+var conceito = "";
+
+if (media >= 8) {
+  conceito = "otimo";
+} else if (media >= 6.5) {
+  conceito = "bom";
 } else {
-  console.log("não poder");
-  console.log("volte futuramente");
+  conceito = "regular";
+}
+console.log(media);
+console.log(conceito);
+
+switch (conceito) {
+  case "ótimo":
+    console.log("Parabéns, vc é um bom aluno");
+    break;
+  case "bom":
+    console.log("você está quase perfeito, estude mais um pouco.");
+    break;
+  case "regular":
+    console.log("Estude mais um pouco, ainda muit ruim");
+    break;
+  default:
+    console.log("houve algum erro");
+    break;
 }
