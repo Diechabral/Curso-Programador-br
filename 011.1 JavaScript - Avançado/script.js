7e7bdab5d4db401aeac42a4cf9798420c3535e66
Diechabral <th3d1eg0master@gmail.com>
2021-11-13 04:00:58 -0300

    updatge

--- 011.1 JavaScript - Avançado/script.js
+++ 011.1 JavaScript - Avançado/script.js
@@ -1,8 +1,33 @@
-"use strict";
 
-function imprimir() {
-     var x = 7
-    console.log(x)
-}
-imprimir();
-console.log(window)
+
+// let dobro = x => {
+
+//     return (2 * x);
+// }
+
+// console.log(dobro(8))
+
+
+
+// o Arrow não reconhece o Bind nem o this... ela reconheceria eles de forma global e não local no arrow.
+
+
+
+
+let double = x => (2 * x);
+
+console.log(double(3))
+
+
+
+
+// let double = () => { console.log(this) }
+
+// let number = {
+//     x: 8,
+//     d: double
+// }
+// double = double.bind(number)
+
+// number.d();
+
